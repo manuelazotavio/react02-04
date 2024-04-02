7d32bc9d6c0bd0ab7e9348245e6b561779e3abea
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-04-02 16:32:37 -0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
-import './App.css';
-import Home from './components/pages/Home'
-import Contato from './components/pages/Contato'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/pages/Home";
+import Contato from "./components/pages/Contato";
+import Container from "./components/layouts/Container";
 
 function App() {
   return (
     <Router>
       <ul>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/contato'}>Contato</Link>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/contato"}>Contato</Link>
       </ul>
-      <Routes>
-        <Route exact path='/' element={Home}>
-  
-        </Route>
-        <Route path='/contato' element={Contato}>
-         
-        </Route>
-      </Routes>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/contato" element={<Contato />}></Route>
+        </Routes>
+      </Container>
+      <p>Footer</p>
     </Router>
   );
 }
---
+++ src/components/layouts/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar(props) {
+  return (
+    <div>
+      <p>oi</p>
+    </div>
+  )
+}
+
+export default NavBar
+
