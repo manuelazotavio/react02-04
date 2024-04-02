2bd22d67b30ce1e01fc7f9bb1b6d402f099e0d1e
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-04-02 15:38:16 -0300

    add link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import './App.css';
 import Home from './components/pages/Home'
 import Contato from './components/pages/Contato'
@@ -7,19 +7,15 @@ function App() {
   return (
     <Router>
       <ul>
-        <li>
-          Home
-        </li>
-        <li>
-          Contato
-        </li>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/contato'}>Contato</Link>
       </ul>
       <Routes>
-        <Route exact path='/' >
-          <Home />
+        <Route exact path='/' element={Home}>
+  
         </Route>
-        <Route path='/contato'>
-          <Contato />
+        <Route path='/contato' element={Contato}>
+         
         </Route>
       </Routes>
     </Router>
