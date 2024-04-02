ad4b0480832743240fd8da26bc936e4f649ca9e2
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-04-02 15:36:10 -0300

    add pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
+import Home from './components/pages/Home'
+import Contato from './components/pages/Contato'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <li>
+          Home
+        </li>
+        <li>
+          Contato
+        </li>
+      </ul>
+      <Routes>
+        <Route exact path='/' >
+          <Home />
+        </Route>
+        <Route path='/contato'>
+          <Contato />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
